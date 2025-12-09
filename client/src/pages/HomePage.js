2c4147504b5c0105b7da453f13f6d3fcffe289df
himanshiwad20 <146841714+himanshiwad20@users.noreply.github.com>
2025-12-10 02:49:58 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,6 @@ import {Prices} from '../components/Prices'
 import toast from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 import { useCart } from '../context/cart'
-import { AiOutlineReload } from "react-icons/ai";
 import "../styles/HomePage.css";
 
 
